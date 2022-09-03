a06431be26c384ee83bddcaf2fae83cb3ec9db20
paulius-str <paulius.st2@gmail.com>
2022-09-03 21:12:30 +0300

    initial commit

---
+++ ClockAngleCounter/Extensions/TimeOnlyExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockAngleCounter.Extensions
+{
+    public static class TimeOnlyExtensions
+    {
+        public static float DegreesBetweenArrows(this TimeOnly time)
+        {
+            float angleMins = 6f * time.Minute;
+            float angleHours = 30f * time.Hour + 0.5f * time.Minute;
+
+            float angleBetween = Math.Abs(angleHours - angleMins);
+
+            if(angleBetween > 180f)
+                angleBetween = 360f - angleBetween;
+
+            return angleBetween;
+        }
+    }
+}
---
+++ ClockAngleCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClockAngleCounter.Extensions;
+
+bool running = true;
+
+while (running)
+{
+    Console.WriteLine("Enter hours");
+    int hours = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter minutes");
+    int minutes = int.Parse(Console.ReadLine());
+
+    TimeOnly time = new TimeOnly(hours, minutes);
+
+    float degreesBetweenArrows = time.DegreesBetweenArrows();
+    Console.WriteLine($"Degrees Between Arrows: {degreesBetweenArrows}");
+}
